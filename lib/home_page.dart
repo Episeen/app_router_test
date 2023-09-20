aedf2bbb6b0b88badb1ea0c2d73c0f85ab1fd784
Lidia Dyachkovskaya <lyatlasova@gmail.com>
2023-09-20 20:07:07 +0600

    #1 first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home Page',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: 'Cart',
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('home page'),
+      ),
+    );
+  }
+}
